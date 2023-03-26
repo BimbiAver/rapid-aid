75a852257e8a665409276b5e1ec877d83be57b71
BimbiAver <deshangoldcity@gmail.com>
2023-03-26 12:35:54 +0530

    Modify the index.js

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 // Define route handlers
 app.use('/', async (req, res) => {
   try {
-    return res.status(200).send();
+    return res.status(200).send("Hey I'm the homepage");
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
